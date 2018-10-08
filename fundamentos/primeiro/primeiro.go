f66e66ea6e533b30d4f8a65312ea05198af40486
Charlie Anacleto Poletti Amadeu <charlie@lnxcit020134.cit>
2018-10-08 09:43:29 -0300

    primeiro

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,12 @@
+//executaveis iniciam pelo main
+package main
+
+/*
+import da função para reliazar o print na tela
+*/
+import "fmt"
+
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
